55d5140ec489d22dc54f9a09773fb785fb877a0c
Justin Lambert <justin.lambert@bigcommerce.com>
2013-10-09 14:12:59 +1100

    take one

--- lib/couchdb_migrate_mastfish.rb
+++ lib/couchdb_migrate_mastfish.rb
@@ -1,5 +1,35 @@
 class Couchdb_migrate_mastfish
-  def self.hi
-    puts "Hello world!"
+
+  def initialize(config)
+    @db = config["db"]
+    @views = config["views"]
+    p @db
+    p 'here'
   end
+
+  def update(http, iter)
+    item = JSON.parse http.response
+    item = transform(item)
+    put = EventMachine::HttpRequest.new(@db + item["_id"]).aput :body => item.to_json
+    put.callback do
+      iter.next
+    end
+  end
+
+  EM.synchrony do
+    concurrency = 50
+    urls = db.view('plays/all')
+
+    # iterator will execute async blocks until completion, .each, .inject also work!
+    results = EM::Synchrony::Iterator.new(urls["rows"], concurrency).each do |url, iter|
+      # fire async requests, on completion advance the iterator
+      http = EventMachine::HttpRequest.new(@db + url["id"]).aget
+      http.callback { update(http, iter) }
+    end
+    EventMachine.stop
+  end
+
 end
+
+
+
